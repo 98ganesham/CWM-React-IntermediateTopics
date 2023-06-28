9e1721cff5491a0f04ef5707336d7d3f78f09d94
98Ganesham <98ganesham@gmail.com>
2023-06-28 14:25:07 +0630

    customizing query settings

--- react2/react-app/src/main.tsx
+++ react2/react-app/src/main.tsx
@@ -6,7 +6,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(
+  
+);
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
