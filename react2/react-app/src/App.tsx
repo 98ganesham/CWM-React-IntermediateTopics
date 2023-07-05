29a1b80195e9dbb0b5adf793272d59210608445c
98Ganesham <98ganesham@gmail.com>
2023-07-05 11:50:19 +0630

    creating a provider

--- react2/react-app/src/App.tsx
+++ react2/react-app/src/App.tsx
@@ -7,20 +7,21 @@ import TasksContext from './state-management/contexts/tasksContext';
 import NavBar from './state-management/NavBar';
 import HomePage from './state-management/HomePage';
 import AuthProvider from './state-management/AuthProvider';
+import TaskProvider from './state-management/TaskProvider';
 
 
 function App() {
    const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  
+   
   
   return (
     
       <AuthProvider>
-        <TasksContext.Provider value={{tasks, dispatch: tasksDispatch}}>
+        <TaskProvider>
           <NavBar />
           <HomePage />
-        </TasksContext.Provider>
-      </AuthProvider>
+          </TaskProvider>
+        </AuthProvider>
     
   );
 
