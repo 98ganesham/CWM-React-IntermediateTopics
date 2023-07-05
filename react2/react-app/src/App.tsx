682412120f0b00031ce64970399ff8ddfd2d927e
98Ganesham <98ganesham@gmail.com>
2023-07-05 11:10:32 +0630

    Exercise

--- react2/react-app/src/App.tsx
+++ react2/react-app/src/App.tsx
@@ -1,21 +1,25 @@
 import { useReducer } from 'react';
-import './App.css'
-import LoginStatus from './state-management/LoginStatus'
+import './App.css';
+import authReducer from './state-management/reducers/authReducer';
 import tasksReducer from './state-management/reducers/tasksReducer';
+import AuthContext from './state-management/contexts/authContext';
+import TasksContext from './state-management/contexts/tasksContext';
 import NavBar from './state-management/NavBar';
 import HomePage from './state-management/HomePage';
-import TasksContext from './state-management/contexts/tasksContext';
 
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
-
+   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
+  const [user, authDispatch] = useReducer(authReducer, '');
+  
   return (
     
-      <TasksContext.Provider value={{tasks, dispatch}}>
-        <NavBar />
-        <HomePage />
-      </TasksContext.Provider>
+      <AuthContext.Provider value={{user,  dispatch: authDispatch}}>
+        <TasksContext.Provider value={{tasks, dispatch: tasksDispatch}}>
+          <NavBar />
+          <HomePage />
+        </TasksContext.Provider>
+      </AuthContext.Provider>
     
   );
 
@@ -24,4 +28,4 @@ function App() {
         
 }
 
-export default App
+export default App;
--- react2/react-app/src/state-management/TaskList.tsx
+++ react2/react-app/src/state-management/TaskList.tsx
@@ -1,14 +1,16 @@
 import { useContext, useReducer, useState } from 'react';
 import tasksReducer from './reducers/tasksReducer';
 import TasksContext from './contexts/tasksContext';
+import AuthContext from './contexts/authContext';
 
 
 
 const TaskList = () => {
     const {tasks, dispatch} =useContext(TasksContext);
-    
+   const{user}= useContext (AuthContext);
   return (
     <>
+    <p>User: {user}</p>
       <button
         onClick={() =>
           dispatch({type: 'ADD', task: {id: Date.now(), title: 'Task' + Date.now()}})}
