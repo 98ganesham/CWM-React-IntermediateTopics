d1c671291e384da02d25b8d9b0c07c311bddee96
98Ganesham <98ganesham@gmail.com>
2023-07-05 16:21:17 +0630

    PART-2

--- react2/react-app/src/main.tsx
+++ react2/react-app/src/main.tsx
@@ -1,13 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routing/router';
+
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
